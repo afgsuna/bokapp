8fa13c11da935166892822fb0472974e32331d82
Sasan <sasan@Sasans-MacBook-Pro.local>
2026-05-10 14:36:36 +0200

    Fix change detection for books

--- Desktop/PraktikTest/bokapp/BookApi/obj/Debug/net10.0/BookApi.AssemblyInfo.cs
+++ Desktop/PraktikTest/bokapp/BookApi/obj/Debug/net10.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14089539f4253d8d51a86abda14444aad40356c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1ff492c792386835fd88ff8d764185202df5789")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
